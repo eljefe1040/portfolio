64c3b568af68ac15cf73fd0b325485d49030cf9c
eljefe <jeff.clairmont@gmail.com>
2015-10-22 17:44:46 -0400

    third commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
        #  This is added to for paperclip gem and imagemagick
-has_attached_file :post_img , styles: { post_index: "250x250>", post_show: "325x325>" }, default_url: "/images/:style/missing.png"
+has_attached_file :post_img , styles: { post_index: "250x250>", post_show: "450x450>"}, default_url: "/images/:style/missing.png"
 validates_attachment_content_type :post_img, content_type: /\Aimage\/.*\Z/
 
 end
